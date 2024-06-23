9897c17057aa3807cff07fcc9f08a46a8bdc7e86
Anitha2902 <nagisettianitha29@gmail.com>
2024-06-23 15:20:05 +0530

    Add files via upload

---
+++ arithmetic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arithmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            char operation;
+            Console.WriteLine("enter first number:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("input operation:");
+            operation = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("enter second number:");
+            y = Convert.ToInt32(Console.ReadLine());
+            if (operation == '+')
+                Console.WriteLine("{0}+{1}={2}", x, y, x + y);
+            else if (operation == '-')
+                Console.WriteLine("{0}-{1}={2}", x, y, x - y);
+            else if (operation == '*')
+                Console.WriteLine("{0}*{1}={2}", x, y, x * y);
+            else if (operation == '/')
+                Console.WriteLine("{0}/{1}={2}", x, y, x / y);
+            else
+                Console.WriteLine("wrong");
+        }
+    }
+
+
+}
+
