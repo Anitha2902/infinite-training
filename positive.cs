0294692012bf4c42d3c2b692d5f876b7cc46fd59
Anitha2902 <nagisettianitha29@gmail.com>
2024-06-23 16:29:18 +0530

    Add files via upload

---
+++ positive.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace positive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+
+            if (num > 0)
+            {
+                 Console.WriteLine("the number is positive.");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine("the number is negative.");
+            }
+            else
+            {
+                Console.WriteLine("the number is zero.");
+            }
+        }
+    }
+}
