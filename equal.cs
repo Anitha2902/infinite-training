8cf0f63e5b2b91c847da56fb468be8d9433cfb6e
Anitha2902 <nagisettianitha29@gmail.com>
2024-06-23 15:47:28 +0530

    Add files via upload

---
+++ equal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class equal
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter first integer: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter second integer:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 == num2)
+            {
+                Console.WriteLine("the integers are equal.");
+            }
+            else
+            {
+                Console.WriteLine("the integers are not equal.");
+            }
+        }
+    }
+}
+
+
+   
---
+++ space.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a digit: ");
+            string input = Console.ReadLine();
+
+            //Displaying the number four times with spaces in between
+            Console.WriteLine($"{input} {input} {input} {input}");
+
+            //Displaying the number four times without spaces
+           Console.WriteLine($"{input}{input}{input}{input}");
+
+            //Repeating the display
+            Console.WriteLine($"{input} {input} {input} {input}");
+            Console.WriteLine($"{input}{input}{input}{input}");
+
+
+        }
+    }
+}
